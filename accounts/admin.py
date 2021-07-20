72901c28671553e5fc65e89bdd7ba1bcb2431282
DireWolf707 <rahulsood707@gmail.com>
2021-07-20 23:40:33 +0530

    added user auth and updated custom user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ['username', 'email']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'courses.apps.CoursesConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'courses:home'
+LOGOUT_REDIRECT_URL = 'courses:home'
