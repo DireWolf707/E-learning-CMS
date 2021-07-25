a5982721840cd98dd9021daa5f7d01c1f687be57
DireWolf707 <rahulsood707@gmail.com>
2021-07-25 19:19:19 +0530

    modified custom user

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,11 +6,11 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'first_name', 'last_name')
 
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'first_name', 'last_name')
